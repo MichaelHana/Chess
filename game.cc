88c867c0d14ab96f967b1b7e614332279aee48a0
cactreacy <118782342+cactreacy@users.noreply.github.com>
2022-12-01 15:18:39 -0500

    Update game.cc

--- game.cc
+++ game.cc
@@ -4,62 +4,68 @@
 #include <string>
 #include <iostream>
 
-void updateViewers() {
+void Game::updateViewers() {
         char cur[rows][cols] = board.getState();
         for (Viewer *viewer: viewers) {
                 viewer.update(cur);
         }
 }
-void play(std::string white-player, std::string black-player) {
-        board = new Board(start);
-        /*for (int i=0; i<players;++i) {
-                
-        }*/ // should maybe see about changing implementation to allow for more players??
-        switch(white-player) {
-                case "human":
-                        Human *human = new Human();
-                players.emplace_back(human);
-                case "computer1":
-                        Computer1 *computer = new Computer1(); // what arguments??
-                        players.emplace_back(computer);
-                case "computer2":
-                        Computer2 *computer = new Computer2(); // what arguments??
-
-                case "computer3":
-
-                case "computer4":
-
-                default:
-                        //throw error to be caught in main?             
-        }
-        switch(black-player) {
-                case "human":
-
-                case "computer1":
-
-                case "computer2":
-          
-                case "computer3":
-
-                case "computer4":
-
-                default:
-                        //throw error to be caught in main? delete necessary memory to avoid leaks?
-        }
-        Text *text = new Text(); //parameters?? - make sure to send board layout, dimensions
-        viewers.emplace_back(text);
-        Graphics *graphic = new Graphic();//
-        viewers.emplace_back(graphic);
-        
+void Game::play() {
         std::string command;
-        while (std::cin >> command) {
-                switch(command) {
+                        while (std::cin >> command)
+                                //delete current board, players, viewers(??)?
+                                switch(command) {
+                                        case "game":
+                                                std::string player1;
+                                                std::string player2;
+                                                std::cin >> 
+                                                board = new Board(start);
+                                        /*for (int i=0; i<players;++i) {
+
+                                        }*/ // should maybe see about changing implementation to allow for more players??
+                                        switch(player1) {
+                                                case "human":
+                                                        Human *human = new Human();
+                                                players.emplace_back(human);
+                                                case "computer1":
+                                                        Computer1 *computer = new Computer1(); // what arguments??
+                                                        players.emplace_back(computer);
+                                                case "computer2":
+                                                        Computer2 *computer = new Computer2(); // what arguments??
+
+                                                case "computer3":
+
+                                                case "computer4":
+
+                                                default:
+                                                        //throw error to be caught in main?             
+                                        }
+                                        switch(black-player) {
+                                                case "human":
+
+                                                case "computer1":
+
+                                                case "computer2":
+
+                                                case "computer3":
+
+                                                case "computer4":
+
+                                                default:
+                                                        //throw error to be caught in main? delete necessary memory to avoid leaks?
+                                        }
+                                        Text *text = new Text(); //parameters?? - make sure to send board layout, dimensions
+                                        viewers.emplace_back(text);
+                                        Graphics *graphic = new Graphic();//
+                                        viewers.emplace_back(graphic);
+                                
                         case "resign":
                                 if (turn%2) {
                                         //return other player win
                                 } else {
                                         //return other player win
                                 }
+                                break;
                         case "move":
                                 if (turn%2) {
                                         board.listmoves(); // what type do I put this in?
@@ -70,11 +76,16 @@ void play(std::string white-player, std::string black-player) {
                                         //do same for other player
                                 }
                                 checkEnd();
+                                break;
                         case "setup":
                                 setup();
+                                break;
+                        default:
+                                std::cout << command << " is not a valid command" << std::endl;
                 }
         }
-
+        // print final score, white wins and black wins
+        // delete stuff? Or will it all be deleted in destructor?
 }
 void setup() {
         std::string command;
@@ -95,6 +106,7 @@ void setup() {
 int checkEnd() {
         
 }
+
 
             
                 
