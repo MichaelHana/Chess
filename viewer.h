46718ddb1f6cb39024718a2485c02128246de87b
cactreacy <118782342+cactreacy@users.noreply.github.com>
2022-12-01 13:36:23 -0500

    Update viewer.h

--- viewer.h
+++ viewer.h
@@ -1,7 +1,13 @@
-#ifndef
+#ifndef VIEWER_H
 #define VIEWER_H
 class Viewer {
+  protected:
+        // need to somehow ensure this will always match the board size in main
+        static const int rows=8;
+        static const int cols=8;
   public:
-  virtual void update(char[][]board)=0;
+        // must either ensure will always be getting 2d char array of proper size or put in protections
+        virtual void update(char board[rows][cols])=0;
+        virtual ~Viewer();
 };
 #endif
