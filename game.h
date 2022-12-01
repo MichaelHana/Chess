b51f27b9d5e6f8d834b206d693b2752c66ee4b14
cactreacy <118782342+cactreacy@users.noreply.github.com>
2022-12-01 15:52:03 -0500

    Update game.h

--- game.h
+++ game.h
@@ -1,17 +1,17 @@
 #include <vector>
 
 class Game {
-        static const int numplayers = 2;
-        static const int rows = 8;
-        static const int cols = 8;
-        float wins[numplayers] = {0,0};
-        char start[rows][cols] = {{'r','n','b','q','k','b','n','r'}, {'p','p','p','p','p','p','p','p'}, {' ',' ',' ',' ',' ',' ',' ',' '}, {' ',' ',' ',' ',' ',' ',' ',' '}, {' ',' ',' ',' ',' ',' ',' ',' '}, {' ',' ',' ',' ',' ',' ',' ',' '}, {'P','P','P','P','P','P','P','P'}, {'R','N','B','Q','K','B','N','R'}};
-        Board *board = nullptr;
-        Vector<Player *> players = nullptr; //?
-        Vector<Viewer *> viewers = nullptr; //?
-        public:
-        void updateViewers();
-        void play();
-        void setup();
-        int checkEnd();
+	static const int numplayers = 2;
+	static const int rows = 8;
+	static const int cols = 8;
+	float wins[numplayers] = {0,0};
+	char start[rows][cols] = {{'r','n','b','q','k','b','n','r'}, {'p','p','p','p','p','p','p','p'}, {' ',' ',' ',' ',' ',' ',' ',' '}, {' ',' ',' ',' ',' ',' ',' ',' '}, {' ',' ',' ',' ',' ',' ',' ',' '}, {' ',' ',' ',' ',' ',' ',' ',' '}, {'P','P','P','P','P','P','P','P'}, {'R','N','B','Q','K','B','N','R'}};
+	Board *board = nullptr;
+	Vector<Player *> players = nullptr; //?
+	Vector<Viewer *> viewers = nullptr; //?
+	public:
+	void updateViewers();
+	void play();
+	void setup();
+	int checkEnd();
 }
